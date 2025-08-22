fcfc56f33072bdb982c0b77bea5800c50684a11f
Munif Tanjim <hello@muniftanjim.dev>
2025-08-22 17:34:03 +0600

    feat(stremio): add signup links for stores

--- internal/stremio/configure/configure_script.go
+++ internal/stremio/configure/configure_script.go
@@ -14,6 +14,36 @@ function onStoreNameChangeUpdateStoreTokenDescription(nameField) {
   if (!nameField || !nameField.options[nameField.selectedIndex].textContent) {
     return;
   }
+  const storeFallback = {
+    alldebrid: "ad",
+    debrider: "dr",
+    debridlink: "dl",
+    easydebrid: "ed",
+    offcloud: "oc",
+    pikpak: "pp",
+    premiumize: "pm",
+    realdebrid: "rd",
+    torbox: "tb",
+    p2p: "p2p",
+  };
+  const nameDescElem = document.querySelector(` + "`[name='${nameField.name}'] + small > span.description`" + `);
+  if (nameDescElem) {
+    const descByStore = {
+      "*": "…",
+      "": "Use <a target='_blank' href='https://github.com/MunifTanjim/stremthru?tab=readme-ov-file#stremthru_store_auth'><code>STREMTHRU_STORE_AUTH</code></a> config",
+      ad: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://alldebrid.com'>Sign Up</a>",
+      dr: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://debrider.app'>Sign Up</a>",
+      dl: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://debrid-link.com/id/4v8Uc'>Sign Up</a>",
+      ed: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://paradise-cloud.com/products/easydebrid'>Sign Up</a>",
+      oc: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://offcloud.com/?=ce30ae1f'>Sign Up</a>",
+      pm: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://www.premiumize.me/register'>Sign Up</a>",
+      pp: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://mypikpak.com/drive/activity/invited?invitation-code=46013321'>Sign Up</a> Invitation Code: <a target='_blank' href='https://mypikpak.com/drive/activity/invited?invitation-code=46013321'><code>46013321</code></a>",
+      rd: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='http://real-debrid.com/?id=12448969'>Sign Up<a>",
+      tb: "<a type='button' class='outline mb-0' style='font-size: 0.75rem; padding: 0.02em 0.5em;' target='_blank' href='https://torbox.app/subscription?referral=fbe2c844-4b50-416a-9cd8-4e37925f5dfa'>Sign Up</a> Referral Code: <a target='_blank' href='https://torbox.app/subscription?referral=fbe2c844-4b50-416a-9cd8-4e37925f5dfa'><code>fbe2c844-4b50-416a-9cd8-4e37925f5dfa</code></a>",
+      p2p: "⚠️ Peer-to-Peer (🧪 Experimental)",
+    };
+    nameDescElem.innerHTML = descByStore[nameField.value] || descByStore[storeFallback[nameField.value]] || descByStore["*"] || "";
+  }
 	const tokenField = document.querySelector(` + tokenSelector + `)
   const tokenDescElem = document.querySelector(` + "`[name='${tokenField.name}'] + small > span.description`" + `);
   if (tokenDescElem) {
@@ -29,19 +59,7 @@ function onStoreNameChangeUpdateStoreTokenDescription(nameField) {
 			pp: "PikPak <a href='https://mypikpak.com/drive/account/basic' target='_blank'>credential</a> in <code>email:password</code> format, e.g. <code>john.doe@example.com:secret-password</code>",
 			rd: "RealDebrid <a href='https://real-debrid.com/apitoken' target='_blank'>API Token</a>",
 			tb: "TorBox <a href='https://torbox.app/settings' target='_blank'>API Key</a>",
-			p2p: "⚠️ Peer-to-Peer (🧪 Experimental)",
-		};
-		const storeFallback = {
-			alldebrid: "ad",
-			debrider: "dr",
-			debridlink: "dl",
-			easydebrid: "ed",
-			offcloud: "oc",
-			pikpak: "pp",
-			premiumize: "pm",
-			realdebrid: "rd",
-			torbox: "tb",
-		  p2p: "p2p",
+			p2p: "…",
 		};
     tokenDescElem.innerHTML = descByStore[nameField.value] || descByStore[storeFallback[nameField.value]] || descByStore["*"] || "";
     tokenField.disabled = nameField.value === "p2p";
