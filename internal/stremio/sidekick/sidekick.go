605f5eb33c117ab4a55f64ebba1798858ad647f9
Munif Tanjim <hello@muniftanjim.dev>
2025-08-23 13:08:35 +0600

    fix(stremio/sidekick): prefer required genre for hide from board

--- internal/stremio/sidekick/sidekick.go
+++ internal/stremio/sidekick/sidekick.go
@@ -987,7 +987,7 @@ func handleAddonModify(w http.ResponseWriter, r *http.Request) {
 			if name := r.FormValue("catalog_name[" + strconv.Itoa(i) + "]"); name != "" {
 				catalog.Name = name
 			}
-			if hidden := r.FormValue("catalog_hidden[" + strconv.Itoa(i) + "]"); name != "" {
+			if hidden := r.FormValue("catalog_hidden[" + strconv.Itoa(i) + "]"); hidden != "" {
 				if canToggleCatalogBoard(catalog) {
 					toggleCatalogBoard(catalog, hidden == "true")
 				}
