5c41c1b47d21623bf6bb64a8f537b2a49e9b02e4
Munif Tanjim <hello@muniftanjim.dev>
2025-09-19 12:20:04 +0600

    perf(peer_token): cache peer token check

--- internal/peer_token/db.go
+++ internal/peer_token/db.go
@@ -1,6 +1,10 @@
 package peer_token
 
 import (
+	"database/sql"
+	"time"
+
+	"github.com/MunifTanjim/stremthru/internal/cache"
 	"github.com/MunifTanjim/stremthru/internal/db"
 )
 
@@ -12,16 +16,32 @@ type PeerToken struct {
 	CreatedAt db.Timestamp
 }
 
-func IsValid(token string) (bool, error) {
+var peerTokenCache = cache.NewLRUCache[bool](&cache.CacheConfig{
+	Lifetime:      15 * time.Minute,
+	Name:          "peer_token_is_valid",
+	LocalCapacity: 512,
+})
+
+func IsValid(token string) (isValid bool, err error) {
 	if token == "" {
 		return false, nil
 	}
 
+	if peerTokenCache.Get(token, &isValid) {
+		return isValid, nil
+	}
+
 	id := ""
 	row := db.QueryRow("SELECT id FROM "+TableName+" WHERE id = ?", token)
-	if err := row.Scan(&id); err != nil {
+	if err := row.Scan(&id); err != nil && err != sql.ErrNoRows {
 		return false, err
 	}
 
-	return id == token, nil
+	isValid = id == token
+
+	if err := peerTokenCache.Add(token, isValid); err != nil {
+		return false, err
+	}
+
+	return isValid, nil
 }
