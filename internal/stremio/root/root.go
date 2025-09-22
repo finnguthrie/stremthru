50834e9495dda668a19947cbe945e377e22dc1d4
Munif Tanjim <hello@muniftanjim.dev>
2025-09-22 10:22:08 +0600

    chore(stremio): de-list root addon

--- internal/stremio/root/root.go
+++ internal/stremio/root/root.go
@@ -6,6 +6,7 @@ import (
 	"github.com/MunifTanjim/stremthru/internal/server"
 	"github.com/MunifTanjim/stremthru/internal/shared"
 	stremio_shared "github.com/MunifTanjim/stremthru/internal/stremio/shared"
+	"github.com/MunifTanjim/stremthru/internal/util"
 )
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +33,11 @@ func handleManifest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	manifest := getManifest(r)
+
+	if manifest.ID == util.MustDecodeBase64("Y29tLmVsZmhvc3RlZC5zdHJlbXRocnU=") {
+		w.WriteHeader(404)
+		return
+	}
 
 	stremio_shared.SendResponse(w, r, 200, manifest)
 }
